965708643d8946c456596be9c43b5fac7dcb8c59
toukaeru <senkoutoburakki@outlook.jp>
2023-01-16 08:15:02 +0000

    [Add]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,15 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -20,15 +24,27 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
-  def update
-   book = Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book.id)
+
+ def update
+   @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+     
+      render :edit
+    end
  end
-   
+
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to '/books'
+ end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
 end
+
+
